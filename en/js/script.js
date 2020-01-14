8e13e1d3572a727ecfd16b3e5bfb013ac7997078
Marc-Olivier Lalonde <marco.pb28@hotmail.com>
2020-01-14 13:04:14 -0500

    Update script.js

--- en/js/script.js
+++ en/js/script.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
 	var swipe = new Hammer(document)
 
+	if ($(window).width() <= 1100) {
+		swipe.on("panright  panleft", function(e) {
+			e.preventDefault()
+			if (e.type == "panright") {
+				// open menu
+				$("#nav-placeholder").css("transform", "translateX(0px)")
+				$("#homeBody").css("margin-left", "0px")
+			} else {
+				// close/hide menu
+				$("#nav-placeholder").css("transform", "translateX(-60px)")
+				$("#homeBody").css("margin-left", "-60px")
+			}
+		})
+	} else {
+		$("#nav-placeholder").css("transform", "translateX(0px)")
+		$("#homeBody").css("margin-left", "0px")
+	}
+
 	$(window).resize(function() {
 		if ($(window).width() <= 1100) {
 			swipe.on("panright  panleft", function(e) {
